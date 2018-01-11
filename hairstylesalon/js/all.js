22ff86d71c56520b8498067f74f422387a2ffe39
yuski.tw@gmail.com <yuski.tw@gmail.com>
2018-01-11 17:20:20 +0800

    hairstyle rwd and jade fixed

---
+++ hairstylesalon/js/all.js
@@ -0,0 +1,9 @@
+'use strict';
+
+$(document).ready(function () {
+   $('.showmenu').on('click', function (e) {
+      e.preventDefault();
+      $('body').toggleClass('menu-show');
+   });
+});
+//# sourceMappingURL=all.js.map
