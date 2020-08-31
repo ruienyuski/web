08391bc32c2d578ac221182e6839ede8b95ad663
ruienyuski <yuski.tw@gmail.com>
2020-08-31 21:00:12 +0800

    remove web

--- hairstylesalon/js/all.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-$(document).ready(function () {
-   $('.showmenu').on('click', function (e) {
-      e.preventDefault();
-      $('body').toggleClass('menu-show');
-   });
-});
-//# sourceMappingURL=all.js.map
